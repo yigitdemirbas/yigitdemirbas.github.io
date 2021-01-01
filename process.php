33031d4d123811226ff25857712be8ea643422c2
Yiğit Demirbaş <y.demirbas@emakina.com.tr>
2021-01-02 02:07:27 +0300

    Test for contact email

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "mr3essa@gmail.com";
+    $to = "demirbasyigit@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
@@ -12,13 +12,9 @@
     $fields{"phone"} = "phone";
     $fields{"message"} = "message";
 
-    
-
-    $body = "Here is what was sent:\r\n"; 
+    $body = "Here is what was sent:\r\n";
 
     foreach($fields as $a => $b){$body .= $b." : ".$_REQUEST[$a]."\r\n"; }
 
-
     $send = mail($to, $subject, $body, $headers);
-
 ?>
